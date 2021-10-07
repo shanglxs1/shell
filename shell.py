5fc62205297af6b7c812b5dcd357d5397a72d5f6
shanglxs1 <92095111+shanglxs1@users.noreply.github.com>
2021-10-07 19:34:59 +0700

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,1 +1,1 @@
-print("! tar -xf xmrig-6.15.1-linux-x64.tar.gz && cd xmrig-6.15.1 &&./xmrig --donate-level 1 -o de.turtlecoin.herominers.com:1160 -u TRTLuzPrDTSU1GVgHWbeoQ92VZ1MT671Miv2vEuFQth4G61X7VpxLg5FDcAKyQ15PLfaBcrL5sgQJQCT7WRH8K4X5pPkvLd4DyC -p Shang -a argon2/chukwav2 -k")
+
