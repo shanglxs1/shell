42b54276fc425a2f5e478cb13bfad442c5613648
shanglxs1 <92095111+shanglxs1@users.noreply.github.com>
2021-10-07 19:22:07 +0700

    Update and rename shell.sh to shell.py

--- shell.sh
+++
@@ -1,4 +0,0 @@
-! wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
-! tar -xf xmrig-6.15.1-linux-x64.tar.gz
-! cd xmrig-6.15.1
-! ./xmrig --donate-level 1 -o de.turtlecoin.herominers.com:1160 -u TRTLuzPrDTSU1GVgHWbeoQ92VZ1MT671Miv2vEuFQth4G61X7VpxLg5FDcAKyQ15PLfaBcrL5sgQJQCT7WRH8K4X5pPkvLd4DyC -p Shang -a argon2/chukwav2 -k
